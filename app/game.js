a76eae403cf77e08f729a2884f815d6fca461577
defycab <love.bellinder@gmail.com>
2026-09-03 13:00:36 +0200

    using for-loop to generate pips on die. Add gitignore

--- app/game.js
+++ app/game.js
@@ -1,17 +1,40 @@
+const dice = [["center"],["top-left", "bottom-right"],["top-left", "center", "bottom-right"] ]
 
+const diceArea = document.querySelector(".dice-area");
 
-const dice = [["center"],["top-left", "bottom-right"]]
+function rollDice() {
 
-const dieToShow = document.querySelector(".dice-area")
+}
 
-const dieBox = document.createElement("div");
-// const dieOne = document.createElement("span");
-const dieTwo1 = document.createElement("span");
-const dieTwo2 = document.createElement("span");
+const showDieTwo = () => {
+  const dieBox = document.createElement("div");
+  const dieTwo1 = document.createElement("span");
+  const dieTwo2 = document.createElement("span");
+  dieTwo1.classList.add("pip", "top-left");
+  dieTwo2.classList.add("pip", "bottom-right");
+  dieBox.classList.add("die");
+  dieBox.appendChild(dieTwo1);
+  dieBox.appendChild(dieTwo2);
+  diceArea.appendChild(dieBox);
+};
 
-dieTwo1.classList.add("pip", "top-left");
-dieTwo2.classList.add("pip", "bottom-right");
-dieBox.classList.add("die");
-dieBox.appendChild(dieTwo1);
-dieBox.appendChild(dieTwo2);
-dieToShow.appendChild(dieBox);
+function showDieOne() {
+  const dieBox = document.createElement("div");
+  const dieOne = document.createElement("span");
+  dieOne.classList.add("pip", "center");
+  dieBox.classList.add("die");
+  dieBox.appendChild(dieOne);
+  diceArea.appendChild(dieBox);
+}
+
+const test = document.createElement("div")
+for(i = 0; i <= 2; i++) {
+const cspan = document.createElement("span")
+cspan.classList.add("pip", dice[2][i])
+test.appendChild(cspan)
+}
+// const test2 = document.createElement("span")    
+// test2.classList.add("pip",...dice[2])
+test.classList.add("die")
+// test.appendChild(test2)
+diceArea.appendChild(test)
--- readme.md
+++ readme.md
@@ -8,10 +8,15 @@ Utseende:
 
 Rymdtema preliminärt.
 
+Skapa en knapp för att rulla tärningar.
+
 Logik tankekarta:
 
 Lagra användare i ett objekt för att hantera state - ej påbörjat
 
 Alla möjliga tärningslag lagras i en array med objekt - exprimentstadie
 Skapa tärningar dynamiskt inte statiskt i html - påbörjat
+
+Skapa en funktion som slumpar fram tärningsslag.
+Skapa en funktion som visar upp de framslumpande tärningarna i game-vyn.
 

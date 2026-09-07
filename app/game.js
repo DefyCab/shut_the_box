6868888715464391c2897734ba63b1c1f6eb4546
defycab <love.bellinder@gmail.com>
2026-09-07 23:52:28 +0200

    add clickcounter and if statement to ensure two tiles can be selected

--- app/game.js
+++ app/game.js
@@ -31,11 +31,18 @@ renderTiles();
 // Jag tänkte att en array var lättare att jobba med men går tilbaka till nodelist igen
 const tiles = document.querySelectorAll(".number");
 
+let clickCounter = 0;
+
 function selectTile(tile) {
+  if (clickCounter === 2 && !tile.classList.contains("number-selected")) {
+    return;
+  }
   if (tile.classList.contains("number-selected")) {
     tile.classList.remove("number-selected");
+    clickCounter--;
   } else {
     tile.classList.add("number-selected");
+    clickCounter++;
   }
 }
 
